f5a7cc83b4ca85fc1d507c4edcd6cd4fee0b4e80
JT-Tsai <41375007h@gapps.ntnu.edu.tw>
2025-01-28 21:09:24 +0800

    train_test

---
+++ PSNR/__init__.py
@@ -0,0 +1,18 @@
+import torch
+import torch.nn as nn
+import math
+
+def psnr(sr, hr, PIXEL_MAX = 1.0):
+    mse = torch.mean((sr - hr) ** 2)
+    if mse == 0:
+        return math.inf
+    else:
+        return 20 * math.log10(PIXEL_MAX / math.sqrt(mse))
+    
+class PSNR(nn.Module):
+    def __init__(self, PIXEL_MAX = 1.0):
+        super(PSNR, self).__init__()
+        self.PIXEL_MAX = PIXEL_MAX
+
+    def forward(self, sr, hr):
+        return psnr(sr, hr, self.PIXEL_MAX)
